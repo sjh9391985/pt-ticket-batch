3f122d1da40a823cf377a88049398b445a36e744
sjh9391985 <sjh9391985@gmail.com>
2024-01-09 21:17:11 +0900

    feat: "생성일 기준으로 데이터 조회 테스트"

--- src/main/java/com/example/repository/packaze/PackageRepository.java
+++ src/main/java/com/example/repository/packaze/PackageRepository.java
@@ -1,6 +1,12 @@
 package com.example.repository.packaze;
 
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface PackageRepository extends JpaRepository<PackageEntity, Integer> {
+    List<PackageEntity> findByCreatedAtAfter(LocalDateTime dateTime, Pageable packageSeq);
 }
--- src/test/java/com/example/repository/PackageRepositoryTest.java
+++ src/test/java/com/example/repository/PackageRepositoryTest.java
@@ -7,7 +7,12 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.ActiveProfiles;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Slf4j
 @SpringBootTest
@@ -26,5 +31,23 @@ public class PackageRepositoryTest {
         packageRepository.save(packageEntity);
 
         Assertions.assertNotNull(packageEntity.getPackageSeq());
+    }
+
+    @Test
+    void test_findByCreatedAtAfter() {
+        LocalDateTime dateTime = LocalDateTime.now().minusMinutes(1);
+
+        PackageEntity packageEntity1 = new PackageEntity();
+        packageEntity1.setPackageName("바디 챌린지 PT 3개월");
+        packageEntity1.setPeriod(90);
+        packageRepository.save(packageEntity1);
+
+        PackageEntity packageEntity2 = new PackageEntity();
+        packageEntity2.setPackageName("바디 챌린지 6개월");
+        packageEntity2.setPeriod(180);
+        packageRepository.save(packageEntity2);
+
+        final List<PackageEntity> packageEntities = packageRepository.findByCreatedAtAfter(dateTime, PageRequest.of(0, 1, Sort.by("packageSeq").descending()));
+
     }
 }
