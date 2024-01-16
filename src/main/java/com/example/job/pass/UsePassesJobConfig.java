3a09bbe048b0e5a90bff249e0fd39b44a1fc5fa0
sjh9391985 <sjh9391985@gmail.com>
2024-01-16 21:00:50 +0900

    feat: "통과 job 과정 개발"

---
+++ src/main/java/com/example/job/pass/UsePassesJobConfig.java
@@ -0,0 +1,121 @@
+package com.example.job.pass;
+
+import com.example.repository.booking.BookingEntity;
+import com.example.repository.booking.BookingRepository;
+import com.example.repository.booking.BookingStatus;
+import com.example.repository.pass.PassEntity;
+import com.example.repository.pass.PassRepository;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.integration.async.AsyncItemProcessor;
+import org.springframework.batch.integration.async.AsyncItemWriter;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.JpaCursorItemReader;
+import org.springframework.batch.item.database.builder.JpaCursorItemReaderBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+
+import javax.persistence.EntityManagerFactory;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.concurrent.Future;
+
+@Configuration
+public class UsePassesJobConfig {
+    private final int CHUNK_SIZE = 10;
+
+    // @EnableBatchProcessing로 인해 Bean으로 제공된 JobBuilderFactory, StepBuilderFactory
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+    private final EntityManagerFactory entityManagerFactory;
+    private final PassRepository passRepository;
+    private final BookingRepository bookingRepository;
+
+    public UsePassesJobConfig(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory, EntityManagerFactory entityManagerFactory, PassRepository passRepository, BookingRepository bookingRepository) {
+        this.jobBuilderFactory = jobBuilderFactory;
+        this.stepBuilderFactory = stepBuilderFactory;
+        this.entityManagerFactory = entityManagerFactory;
+        this.passRepository = passRepository;
+        this.bookingRepository = bookingRepository;
+    }
+
+    @Bean
+    public Job usePassesJob() {
+        return this.jobBuilderFactory.get("usePassesJob")
+                .start(usePassesStep())
+                .build();
+    }
+
+    @Bean
+    public Step usePassesStep() {
+        return this.stepBuilderFactory.get("usePassesStep")
+                .<BookingEntity, Future<BookingEntity>>chunk(CHUNK_SIZE)
+                .reader(usePassesItemReader())
+                .processor(usePassesAsyncItemProcessor())
+                .writer(usePassesAsyncItemWriter())
+                .build();
+    }
+
+    @Bean
+    public JpaCursorItemReader<BookingEntity> usePassesItemReader() {
+        HashMap<String,Object> map = new HashMap<>();
+        map.put("status", BookingStatus.COMPLETED);
+        map.put("endedAt", LocalDateTime.now());
+        return new JpaCursorItemReaderBuilder<BookingEntity>()
+                .name("usePassesItemReader")
+                .entityManagerFactory(entityManagerFactory)
+                // 상태(status)가 완료이며, 종료 일시(endedAt)이 과거인 예약이 이용권 차감 대상이 됩니다.
+                .queryString("select b from BookingEntity b join fetch b.passEntity where b.status = :status and b.usedPass = false and b.endedAt < :endedAt")
+                .parameterValues(map)
+                .build();
+    }
+
+    @Bean
+    public AsyncItemProcessor<BookingEntity, BookingEntity> usePassesAsyncItemProcessor() {
+        AsyncItemProcessor<BookingEntity, BookingEntity> asyncItemProcessor = new AsyncItemProcessor<>();
+        asyncItemProcessor.setDelegate(usePassesItemProcessor());
+        asyncItemProcessor.setTaskExecutor(new SimpleAsyncTaskExecutor());
+        return asyncItemProcessor;
+    }
+
+    @Bean
+    public ItemProcessor<BookingEntity, BookingEntity> usePassesItemProcessor() {
+        return bookingEntity -> {
+            PassEntity passEntity = bookingEntity.getPassEntity();
+            passEntity.setRemainingCount(passEntity.getRemainingCount() - 1);
+            bookingEntity.setPassEntity(passEntity);
+
+            bookingEntity.setUsedPass(true);
+            return bookingEntity;
+        };
+    }
+
+    @Bean
+    public AsyncItemWriter<BookingEntity> usePassesAsyncItemWriter() {
+        AsyncItemWriter<BookingEntity> asyncItemWriter = new AsyncItemWriter<>();
+        asyncItemWriter.setDelegate(usePassesItemWriter()); // usePassesItemWriter 최종 결과값을 넘겨주고 작업을 위임합니다.
+        return asyncItemWriter;
+    }
+
+
+    @Bean
+    public ItemWriter<BookingEntity> usePassesItemWriter() {
+        return bookingEntities -> {
+            for (BookingEntity bookingEntity : bookingEntities) {
+                // 잔여 횟수를 업데이트 합니다.
+                int updatedCount = passRepository.updateRemainingCount(bookingEntity.getPassSeq(), bookingEntity.getPassEntity().getRemainingCount());
+                // 잔여 횟수가 업데이트 완료되면, 이용권 사용 여부를 업데이트합니다.
+                if (updatedCount > 0) {
+                    bookingRepository.updateUsedPass(bookingEntity.getPassSeq(), bookingEntity.isUsedPass());
+                }
+            }
+        };
+    }
+
+
+
+}
