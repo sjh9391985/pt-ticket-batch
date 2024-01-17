95a89b9b2e86e106192997d8852736451f7650b1
sjh9391985 <sjh9391985@gmail.com>
2024-01-17 22:32:52 +0900

    feat: "view 컨트롤러 추가"

---
+++ src/main/java/com/example/controller/PassViewController.java
@@ -0,0 +1,34 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+@RequestMapping(value = "/passes")
+public class PassViewController {
+    private final UserService userService;
+    private final PassService passService;
+
+    public PassViewController(UserService userService, PassService passService) {
+        this.userService = userService;
+        this.passService = passService;
+    }
+
+    @GetMapping
+    public ModelAndView getPasses(@RequestParam("userId") String userId, ModelAndView modelAndView){
+
+        final List<Pass> passes = passService.getPasses(userId);
+        final User user = userService.getUser(userId);
+
+        modelAndView.addObject("passes", passes);
+        modelAndView.addObject("user", user);
+        modelAndView.setView("pass/index");
+
+        return modelAndView;
+    }
+}
